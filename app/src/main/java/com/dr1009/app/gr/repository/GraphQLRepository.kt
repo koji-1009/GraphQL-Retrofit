63da5062af84d99c921194762220cd4a082b7087
Koji Wakamiya <koji.wakamiya@gmail.com>
2020-08-05 23:48:23 +0900

    Add RepositoriesQuery

--- app/src/main/java/com/dr1009/app/gr/repository/GraphQLRepository.kt
+++ app/src/main/java/com/dr1009/app/gr/repository/GraphQLRepository.kt
@@ -2,11 +2,19 @@ package com.dr1009.app.gr.repository
 
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.coroutines.toDeferred
-import com.dr1009.app.gr.SampleQuery
+import com.dr1009.app.gr.RepositoriesQuery
+import com.dr1009.app.gr.type.OrderDirection
+import com.dr1009.app.gr.type.RepositoryOrderField
 import javax.inject.Inject
 
 class GraphQLRepository @Inject constructor(
     private val client: ApolloClient
 ) {
-    suspend fun sample() = client.query(SampleQuery()).toDeferred().await()
+    suspend fun repositories() = client.query(
+        RepositoriesQuery(
+            repositoriesCount = 20,
+            orderBy = RepositoryOrderField.CREATED_AT,
+            orderDirection = OrderDirection.ASC
+        )
+    ).toDeferred().await()
 }
