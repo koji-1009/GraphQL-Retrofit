22cb011dfd9bd6dbdba2eae0f91a256de82ead0f
Koji Wakamiya <koji.wakamiya@gmail.com>
2020-09-28 06:43:22 +0900

    Remove __typename

--- app/src/main/java/com/dr1009/app/gr/repository/RetrofitRepository.kt
+++ app/src/main/java/com/dr1009/app/gr/repository/RetrofitRepository.kt
@@ -9,7 +9,7 @@ class RetrofitRepository @Inject constructor(
 ) {
     suspend fun repositories() = service.repositories(
         GraphQLBody(
-            query = "query { viewer { __typename repositories(first: 20) { __typename nodes { __typename ...RepositoryFragment } } } } fragment RepositoryFragment on Repository { __typename id name url description createdAt }",
+            query = "query { viewer { repositories(first: 20) { nodes { ...RepositoryFragment } } } } fragment RepositoryFragment on Repository { id name url description createdAt }",
             variables = "{}"
         )
     ).data.viewer.repositories.nodes
